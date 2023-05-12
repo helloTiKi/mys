f0878ea766c59718cbeeb982ebcbd076dc26b3b9
helloTiKi <1440568001@qq.com>
2023-05-12 18:19:24 +0800

    .

---
+++ plugins/genshin/model/httpServer.js
@@ -0,0 +1,195 @@
+import { createServer } from 'http';
+var getFunc = {};
+var postFunc = {};
+/**
+ * 
+ * @param {string} _contentType content-Type字段
+ */
+function getContentType(_contentType) {
+    let retdata = {
+        /**表示主类型，对于text/html，type就是text */
+        type: '',
+        /**表示子类型，对于text/html，subtype就是html */
+        subtype: '',
+        /**表示字符集，比如utf-8； */
+        charset: 'utf-8',
+        /**用于分割消息体中多个实体之间的边界*/
+        boundary: '',
+        /**用于标识消息体的开头部分 */
+        start: '',
+        /**用于指示消息体中使用的自然语言 */
+        language: '',
+        /**可以添加其他的定制属性，这些属性必须以“X-”开头。 */
+        extension: ''
+    }
+
+    _contentType.split(';').forEach(e => {
+        let data = e.toLowerCase();
+        let isOK = false;
+        data.replace(/charset=([\s|\S]*)/g, (_a, value, _c) => {
+            if (['utf8', 'utf-8'].includes(value)) {
+                retdata.charset = 'utf-8'
+            } else retdata.charset = value;
+            isOK = true;
+        })
+        if (isOK) return
+        data.replace(/([application|audio|font|image|message|model|multipart|text|video]*)\/([\s|\S]*)/g, (_a, type, subtype) => {
+            retdata.type = type, retdata.subtype = subtype;
+            isOK = true;
+        })
+        if (isOK) return
+    })
+    return retdata;
+}
+const server = createServer(function (req, res) {
+    console.log("有客户")
+    if (req.method == 'GET') {
+        {
+            var url = req.url;
+            var params = {};
+            var i = 0;
+            if (i = url.indexOf("?"), i != -1) {
+                let d = url.slice(i + 1, url.length);
+                url = url.slice(0, i)
+                d.split("&").forEach(e => {
+                    let key = e.split("=");
+                    params[key[0]] = key[1]
+                })
+            }
+        }
+        try {
+            getFunc[url](req, res, params)
+        } catch (error) {
+            try {
+                getFunc['default'](req, res, url)
+            } catch (error) {
+                console.error(error)
+                console.error(url + '路径未定义')
+                res.writeHead(404)
+                res.end()
+            }
+        }
+        return
+    } else if (req.method == 'POST') {
+        let data = new Uint8Array();
+        req.on('data', function (d) {
+            data += d;
+            console.log('有数据')
+        })
+        req.on('end', function () {
+            //console.log(data + '')
+            try {
+                let type = getContentType(req.headers['content-type']);
+                switch (type.type) {
+                    case 'application':
+                        switch (type.subtype) {
+                            case 'json':
+                                data = JSON.parse(data + '');
+                                break
+                        }
+                        break
+                }
+                console.log(type)
+                postFunc[req.url](req, res, data)
+            } catch (error) {
+                try {
+                    postFunc['default'](req, res, data)
+                } catch (error) {
+                    console.error(error)
+                    res.writeHead(404)
+                    res.end()
+                }
+            }
+            res.end('success')
+        })
+
+    }
+
+
+    //res.end("success")
+});
+/**
+ * 
+ * @param {http.IncomingMessage} _req 请求数据体
+ * @param {http.ServerResponse} _res 回复数据体
+ * @param {object} param Get请求参数
+ */
+function HttpGet(_req, _res, _params) {
+
+}
+/**
+ * 
+ * @param {http.IncomingMessage} _req 请求数据体
+ * @param {http.ServerResponse} _res 回复数据体
+ * @param {string|Uint8Array|Object} _data post数据体
+ */
+function HttpPost(_req, _res, _data) {
+}
+/**
+ * 
+ * @param {http.IncomingMessage} _req 
+ * @param {http.ServerResponse} _res 
+ * @param {string} _url 
+ */
+function HttpDefaultByGet(_req, _res, _url) {
+}
+/**
+ * 
+ * @param {http.IncomingMessage} _req 
+ * @param {http.ServerResponse} _res 
+ * @param {string|object|Uint8Array} _data 
+ */
+function HttpDefaultByPost(_req, _res, _data) {
+}
+export default class httpServer {
+    constructor(port) {
+        server.listen(port)
+    }
+    /**
+     *
+     * @param {string|string[]} path url请求路径
+     * @param {HttpGet} func 回调函数
+     */
+    get(path, func) {
+        if (Array.isArray(path)) {
+            path.forEach(e => {
+                getFunc[e] = func;
+            })
+        } else getFunc[path] = func;
+
+    }
+    /**
+     *
+     * @param {string|string[]} path
+     * @param {HttpPost} func
+     */
+    post(path, func) {
+        if (Array.isArray(path)) {
+            path.forEach(e => {
+                postFunc[e] = func;
+            })
+        } else postFunc[path] = func;
+    }
+    /**
+     *
+     * @param {string|string[]} method
+     * @param {HttpDefaultByGet|HttpDefaultByPost} func
+     */
+    default(method, func) {
+        function set(key, func) {
+            switch (key) {
+                case 'get':
+                    getFunc['default'] = func;
+                    break;
+                case 'post':
+                    postFunc['default'] = func;
+                    break
+            }
+        }
+        if (Array.isArray(method)) {
+            method.map(str => str.toLowerCase()).forEach(e => {
+                set(e, func)
+            })
+        } else set(key, func);
+    }
+};
--- plugins/genshin/model/mysCapchp.server.js
+++ plugins/genshin/model/mysCapchp.server.js
@@ -1,14 +1,15 @@
-import http from 'http'
+import YAML from 'yaml'
 import CryptoJS from 'crypto-js'
 import fs from 'node:fs'
-//import axios from 'axios'
+
 import axios from 'axios'
+import httpServer from './httpServer'
 //debugger
 let path = './plugins/genshin/resources/html/geetest'
 let port = 4399;
 let isPublicnetwork = false;
 let IPaddress = '127.0.0.1';
-var getHtmlData = typeof getHtmlData == 'function' ? getHtmlData : function e(s) {
+var getHtmlData = typeof getHtmlData == 'function' ? getHtmlData : function e(_s) {
     let data = {};
     function get(key) {
         if (!data[key]) {
@@ -18,6 +19,7 @@ var getHtmlData = typeof getHtmlData == 'function' ? getHtmlData : function e(s)
     }
     return get
 }()
+
 function Captcha() {
     this.CaptchaData = {};
     setTimeout(() => {
@@ -78,108 +80,73 @@ Captcha.prototype = {
         }, 30000);
     }
 }
-if (!global.mysCaptchaServer) {
-    console.log('正在启动验证服务')
-    const server = http.createServer(function (req, res) {
-        if (req.method == 'GET') {
-            {
-                var url = req.url;
-                var params = {};
-                var i = 0;
-                if (i = url.indexOf("?"), i != -1) {
-                    let d = url.slice(i + 1, url.length);
-                    url = url.slice(0, i)
-                    d.split("&").forEach(e => {
-                        let key = e.split("=");
-                        params[key[0]] = key[1]
-                    })
-                }
-            }
-            let data;
-            switch (url) {
-                case '/':
-                case '/index.html':
-                    if (!params.sign) {
-                        captcha.newCaptcha().then(e => {
-                            res.statusCode = 302;
-                            res.setHeader("Location", '/index.html?sign=' + e)
-                            res.end()
-                        })
-                        return
-                    }
-                    data = fs.readFileSync(path + '/index.html', { encoding: 'utf-8' })
-                    res.end(data)
-                    break
-                case '/ret':
-                    data = JSON.parse(req.headers.geetest)
-                    console.log(data)
-                    captcha.setValidate(data)
-                    res.end('OK')
-                    break;
-                case '/getCaptcha':
-                    res.setHeader('content-type', 'application/json; charset=utf-8')
-                    let retdata = {
-                        code: 0,
-                        message: 'success',
-                        data: {}
-                    }
-                    data = captcha.getCaptcha(params.sign);
-                    if (data == undefined) retdata.code = -1, retdata.message = "该sign值不存在或已超时"
-                    retdata.data = data || {}
-                    res.end(JSON.stringify(retdata))
-                    break;
-                case '/Publicnetwork':
-                    res.end('OK')
-                    break;
-                case '/newCaptcha':
-                    data = captcha.setCaptcha(params.gt, params.challenge)
-                    res.end(data)
-                    break;
-                default:
-                    data = getHtmlData(url)
-                    res.setHeader('Last-Modified', 'Mon, 03 Apr 2023 13:37:30 GMT')
-                    console.log(`发送了${url}文件`)
-                    res.end(data)
-
-            }
+var http = new httpServer(port);
+//接口设置区
+{//http.get('/getCaptcha', (req, res, params) => {})
+    http.get(['/', '/index.html'], (_req, res, params) => {
+        if (!params.sign) {
+            captcha.newCaptcha().then(e => {
+                res.statusCode = 302;
+                res.setHeader("Location", '/index.html?sign=' + e)
+                res.end()
+            })
             return
         }
-        console.log("有客户")
-        let data = new Uint8Array();
-        req.on('data', function (d) {
-            data += d;
-            console.log('有数据')
-        })
-        req.on('end', function () {
-            console.log(data + '')
-            captcha.setValidate(JSON.parse(data), params.sign)
-            res.end("success")
-        })
-        //res.end("success")
-    });
-    global.mysCaptchaServer = server
-    global.mysCaptchaServer.listen(port, '',)
-    new Promise(async (res, rej) => {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        let data = (await axios.get('https://qifu-api.baidubce.com/ip/local/geo/v1/district')).data
-        if (data.code == 'Success' || data.msg == "查询成功") {
-            IPaddress = data.ip;
-            axios.get(`http://${data.ip}:${port}/Publicnetwork`, { timeout: 5000 })
-                .then(e => {
-                    isPublicnetwork = true;
-                    console.log(`当前ip:${IPaddress} 为可访问到本机的公网IP`)
-                })
-                .catch(e => {
-                    isPublicnetwork = false;
-                    console.log(`当前ip:${IPaddress} 为无法访问到本机的外网IP\n设置为本地ip:127.0.0.1`)
-                    IPaddress = '127.0.0.1'
-                })
-        } else {
-            console.error('ip接口查询异常')
-            console.info(JSON.stringify(data))
+        data = fs.readFileSync(path + '/index.html', { encoding: 'utf-8' })
+        res.end(data)
+    })
+    http.get('/ret', (req, res, _param) => {
+        data = JSON.parse(req.headers.geetest)
+        console.log(data)
+        captcha.setValidate(data)
+        res.end('OK')
+    })
+    http.get('/getCaptcha', (_req, res, params) => {
+        res.setHeader('content-type', 'application/json; charset=utf-8')
+        let retdata = {
+            code: 0,
+            message: 'success',
+            data: {}
         }
+        data = captcha.getCaptcha(params.sign);
+        if (data == undefined) retdata.code = -1, retdata.message = "该sign值不存在或已超时"
+        retdata.data = data || {}
+        res.end(JSON.stringify(retdata))
+    })
+    http.get('/Publicnetwork', (_req, res, _params) => {
+        res.end('OK')
+    })
+    http.get('/newCaptcha', (_req, res, params) => {
+        data = captcha.setCaptcha(params.gt, params.challenge)
+        res.end(data)
+    })
+    http.default('get', (req, res, url) => {
+        data = getHtmlData(url)
+        res.setHeader('Last-Modified', 'Mon, 03 Apr 2023 13:37:30 GMT')
+        console.log(`发送了${url}文件`)
+        res.end(data)
     })
 }
+new Promise(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    let data = (await axios.get('https://qifu-api.baidubce.com/ip/local/geo/v1/district')).data
+    if (data.code == 'Success' || data.msg == "查询成功") {
+        IPaddress = data.ip;
+        axios.get(`http://${data.ip}:${port}/Publicnetwork`, { timeout: 5000 })
+            .then(e => {
+                isPublicnetwork = true;
+                console.log(`当前ip:${IPaddress} 为可访问到本机的公网IP`)
+            })
+            .catch(e => {
+                isPublicnetwork = false;
+                console.log(`当前ip:${IPaddress} 为无法访问到本机的外网IP\n设置为本地ip:127.0.0.1`)
+                IPaddress = '127.0.0.1'
+            })
+    } else {
+        console.error('ip接口查询异常')
+        console.info(JSON.stringify(data))
+    }
+})
 if (!global.mysCaptchaData) {
     var captcha = new Captcha();
     global.mysCaptchaData = captcha;
